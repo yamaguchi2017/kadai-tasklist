fe890b795c9c22614bd5be0f18734f4f5bd037c4
yamaguchi2017 <x_osigoto_x@yahoo.co.jp>
2017-06-22 13:50:42 +0000

    TaskList bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,24 @@
 
     <h1>タスク新規作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('status', 'タスクステータス：') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'タスク内容：') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('登録') !!}
-
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'タスクステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク内容:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
